2adcfc641c78f55d006f1361d01a36939c6c5408
Hemanth KV <150644728+ReactwithHemanth@users.noreply.github.com>
2025-04-06 08:40:39 +0530

    Fix:failed build

--- app/api/invoices/route.js
+++ app/api/invoices/route.js
@@ -1,17 +1,17 @@
-import { NextResponse } from "next/server";
-import dbConnect from "@/lib/dbConnect";
-import Invoice from "@/models/Invoice";
+// import { NextResponse } from "next/server";
+// import dbConnect from "@/lib/dbConnect";
+// import Invoice from "@/models/Invoice";
 
-export async function GET() {
-  await dbConnect();
-  const invoices = await Invoice.find({});
-  return NextResponse.json(invoices);
-}
+// export async function GET() {
+//   await dbConnect();
+//   const invoices = await Invoice.find({});
+//   return NextResponse.json(invoices);
+// }
 
-export async function POST(req) {
-  await dbConnect();
-  const { project, client, amount, status, dueDate } = await req.json();
-  const newInvoice = new Invoice({ project, client, amount, status, dueDate });
-  await newInvoice.save();
-  return NextResponse.json(newInvoice);
-}
+// export async function POST(req) {
+//   await dbConnect();
+//   const { project, client, amount, status, dueDate } = await req.json();
+//   const newInvoice = new Invoice({ project, client, amount, status, dueDate });
+//   await newInvoice.save();
+//   return NextResponse.json(newInvoice);
+// }
